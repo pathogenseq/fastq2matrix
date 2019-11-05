eda52a8a2c5acd7c2fabf13875a0652bac2c5720
Jody Phelan <jody.phelan@lshmt.ac.uk>
2019-11-05 14:18:36 +0000

    add vcf class

---
+++ fastq2matrix/vcf.py
@@ -0,0 +1,65 @@
+import os
+from .utils import run_cmd, nofile, get_random_file
+
+
+class vcf_class:
+    def __init__(self,filename,threads=4):
+        self.samples = []
+        self.filename = filename
+        self.threads = threads
+        self.prefix = filename[:-7]
+        if nofile(filename+".csi"):
+            run_cmd("bcftools index  %(filename)s" % vars(self))
+        self.temp_file = get_random_file()
+        run_cmd("bcftools query -l %(filename)s > %(temp_file)s" % vars(self))
+        for l in open(self.temp_file):
+            self.samples.append(l.rstrip())
+        os.remove(self.temp_file)
+
+    def vcf_to_fasta(self,ref_file,threads=4,chunk_size = 50000):
+        self.ref_file = ref_file
+        self.chunk_size = chunk_size
+        self.cmd_split_chr = "bedtools makewindows -g %(ref_file)s.fai -w %(chunk_size)s -s  %(chunk_size)s | awk '{print $1\":\"$2\"-\"$3}'" % vars(self)
+        self.tmp_file = "%s.tmp.txt" % self.prefix
+        self.threads = threads
+        cmd = "%(cmd_split_chr)s | parallel -j %(threads)s \"bcftools view  %(filename)s -r {} -Ou | bcftools query -f '%%POS[\\t%%IUPACGT]\\n' | sed 's/\*[\/|]\*/\.\/\./g' |  datamash transpose > %(prefix)s.{}.tmp.txt\"" % vars(self)
+        run_cmd(cmd)
+        cmd = "paste `%(cmd_split_chr)s | awk '{print \"%(prefix)s.\"$1\".tmp.txt\"}'` > %(tmp_file)s" % vars(self)
+        run_cmd(cmd)
+        cmd = "rm `%(cmd_split_chr)s | awk '{print \"%(prefix)s.\"$1\".tmp.txt\"}'`" % vars(self)
+        run_cmd(cmd)
+        with open(self.prefix+".snps.fa","w") as O:
+            for i,l in enumerate(open(self.tmp_file)):
+                row = l.rstrip().split()
+                if i==0: continue
+                s = self.samples[i-1]
+                seq = "".join(row).replace("./.","N")
+                O.write(">%s\n%s\n" % ( s,seq))
+        run_cmd("rm %s" % self.tmp_file)
+
+    def vcf_to_matrix(self,):
+        self.matrix_file = self.prefix+".mat"
+        self.binary_matrix_file = self.prefix+".mat.bin"
+        O = open(self.matrix_file,"w").write("chr\tpos\tref\t%s\n" % ("\t".join(self.samples)))
+        run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%IUPACGT]\\n' %(filename)s | tr '|' '/' | sed 's/\.\/\./N/g' >> %(matrix_file)s" % vars(self))
+        O = open(self.binary_matrix_file,"w").write("chr\tpos\tref\t%s\n" % ("\t".join(self.samples)))
+        run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%GT]\\n' %(filename)s | tr '|' '/' | sed 's/\.\/\./N/g' | sed 's/1\/1/1/g' | sed 's/0\/0/0/g' >> %(binary_matrix_file)s" % vars(self))
+
+    def get_plink_dist(self,pca=True,mds=True):
+        self.tempfile = get_random_file(extension=".vcf")
+        run_cmd("bcftools view %(filename)s > %(tempfile)s" % vars(self))
+        run_cmd("plink --vcf %(tempfile)s --distance square --double-id --allow-extra-chr --out %(prefix)s.temp" % vars(self))
+        O = open("%(prefix)s.dist" % vars(self),"w")
+        dists = []
+        for l in open("%(prefix)s.temp.dist" % vars(self)):
+            row = [float(d)/2 for d in l.rstrip().split()]
+            O.write("%s\n" % "\t".join([str(x) for x in row]))
+            dists.append(row)
+        O.close()
+        if pca:
+            run_cmd("plink --vcf %(tempfile)s --pca --double-id --allow-extra-chr --out %(prefix)s.pca" % vars(self))
+        if mds:
+            run_cmd("plink --vcf %(tempfile)s --mds-plot 10 eigendecomp --cluster --double-id --allow-extra-chr --out %(prefix)s.pca" % vars(self))
+
+        run_cmd("rm %(tempfile)s* %(prefix)s.temp* %(prefix)s.pca.log %(prefix)s.pca.nosex" % vars(self))
+        return dists
