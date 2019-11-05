805d557e42d555a2c24216189bcf429b7769d8ef
jodyphelan <jody.phelan@lshtm.ac.uk>
2019-11-05 15:14:29 +0100

    add bqsr code

--- scripts/fastq2vcf.py
+++ scripts/fastq2vcf.py
@@ -9,8 +9,14 @@ def main_trim(args):
     fm.run_cmd("trimmomatic PE %(read1)s %(read2)s -baseout %(prefix)s LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36" % vars(args))
 
 def main_map(args):
-    fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(read1)s %(read2)s | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(prefix)s.bam -" % vars(args))
-    fm.run_cmd("samtools index %(prefix)s.bam" % vars(args))
+    fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(prefix)s_1P %(prefix)s_2P | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(prefix)s.bam -" % vars(args))
+    fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bam" % vars(args))
+	fm.run_cmd("rm %(prefix)s_1P %(prefix)s_2P %(prefix)s_1U %(prefix)s_2U")
+	if args.bqsr_vcf:
+		fm.run_cmd("gatk BaseRecalibrator -R %(ref)s -I %(prefix)s.bam --known-sites %(bqsr_vcf)s -O %(prefix)s.recal_data.table")
+		fm.run_cmd("gatk ApplyBQSR -R %(ref)s -I %(prefix)s.bam --bqsr-recal-file %(prefix)s.recal_data.table -O %(prefix)s.bqsr.bam")
+		fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bqsr.bam")
+		fm.run_cmd("rm %(prefix)s.bam")
 
 def main_gatk(args):
     if not args.prefix:
@@ -20,7 +26,7 @@ def main_gatk(args):
 
 def main_all(args):
     main_trim(args)
-    args.bam = args.prefix+".bam"
+    args.bam = args.prefix+".bqsr.bam" if args.bqsr_vcf else args.prefix+".bam"
     main_map(args)
     main_gatk(args)
 
@@ -33,11 +39,10 @@ parser_sub.add_argument('--read2','-2',help='Second read file',required=True)
 parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated',required=True)
 parser_sub.add_argument('--ref','-r',help='Second read file',required=True)
 parser_sub.add_argument('--threads','-t',default=4,help='Number of threads')
+parser_sub.add_argument('--bqsr-vcf','-q',help='VCF file used for bqsr')
 parser_sub.set_defaults(func=main_all)
 
 parser_sub = subparsers.add_parser('trim', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser_sub.add_argument('--read1','-1',help='First read file',required=True)
-parser_sub.add_argument('--read2','-2',help='Second read file',required=True)
 parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated',required=True)
 parser_sub.set_defaults(func=main_trim)
 
@@ -47,6 +52,7 @@ parser_sub.add_argument('--read2','-2',help='Second read file',required=True)
 parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results generated',required=True)
 parser_sub.add_argument('--ref','-r',help='Second read file',required=True)
 parser_sub.add_argument('--threads','-t',default=4,help='Number of threads')
+parser_sub.add_argument('--bqsr-vcf','-q',help='VCF file used for bqsr')
 parser_sub.set_defaults(func=main_map)
 
 parser_sub = subparsers.add_parser('gatk', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
