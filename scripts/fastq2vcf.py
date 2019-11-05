1ec3a67a9d44e0785879634764d9d2adf49413b0
jodyphelan <jody.phelan@lshtm.ac.uk>
2019-11-05 15:17:06 +0100

    add bqsr code

--- scripts/fastq2vcf.py
+++ scripts/fastq2vcf.py
@@ -6,29 +6,29 @@ import fastq2matrix as fm
 
 
 def main_trim(args):
-    fm.run_cmd("trimmomatic PE %(read1)s %(read2)s -baseout %(prefix)s LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36" % vars(args))
+	fm.run_cmd("trimmomatic PE %(read1)s %(read2)s -baseout %(prefix)s LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36" % vars(args))
 
 def main_map(args):
-    fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(prefix)s_1P %(prefix)s_2P | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(prefix)s.bam -" % vars(args))
-    fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bam" % vars(args))
-	fm.run_cmd("rm %(prefix)s_1P %(prefix)s_2P %(prefix)s_1U %(prefix)s_2U")
+	fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(prefix)s_1P %(prefix)s_2P | samtools view -@ %(threads)s -b - | samtools sort -@ %(threads)s -o %(prefix)s.bam -" % vars(args))
+	fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bam" % vars(args))
+	fm.run_cmd("rm %(prefix)s_1P %(prefix)s_2P %(prefix)s_1U %(prefix)s_2U" % vars(args))
 	if args.bqsr_vcf:
-		fm.run_cmd("gatk BaseRecalibrator -R %(ref)s -I %(prefix)s.bam --known-sites %(bqsr_vcf)s -O %(prefix)s.recal_data.table")
-		fm.run_cmd("gatk ApplyBQSR -R %(ref)s -I %(prefix)s.bam --bqsr-recal-file %(prefix)s.recal_data.table -O %(prefix)s.bqsr.bam")
-		fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bqsr.bam")
-		fm.run_cmd("rm %(prefix)s.bam")
+		fm.run_cmd("gatk BaseRecalibrator -R %(ref)s -I %(prefix)s.bam --known-sites %(bqsr_vcf)s -O %(prefix)s.recal_data.table" % vars(args))
+		fm.run_cmd("gatk ApplyBQSR -R %(ref)s -I %(prefix)s.bam --bqsr-recal-file %(prefix)s.recal_data.table -O %(prefix)s.bqsr.bam" % vars(args))
+		fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bqsr.bam" % vars(args))
+		fm.run_cmd("rm %(prefix)s.bam" % vars(args))
 
 def main_gatk(args):
-    if not args.prefix:
-        args.prefix = args.bam.replace(".bam","")
-    fm.run_cmd("gatk HaplotypeCaller -I %(bam)s -R %(ref)s -O %(prefix)s.g.vcf.gz -ERC BP_RESOLUTION" % vars(args))
-    fm.run_cmd("gatk ValidateVariants -V %(prefix)s.g.vcf.gz -gvcf -R %(ref)s && touch %(prefix)s.g.vcf.gz.validated" % vars(args))
+	if not args.prefix:
+		args.prefix = args.bam.replace(".bam","")
+	fm.run_cmd("gatk HaplotypeCaller -I %(bam)s -R %(ref)s -O %(prefix)s.g.vcf.gz -ERC BP_RESOLUTION" % vars(args))
+	fm.run_cmd("gatk ValidateVariants -V %(prefix)s.g.vcf.gz -gvcf -R %(ref)s && touch %(prefix)s.g.vcf.gz.validated" % vars(args))
 
 def main_all(args):
-    main_trim(args)
-    args.bam = args.prefix+".bqsr.bam" if args.bqsr_vcf else args.prefix+".bam"
-    main_map(args)
-    main_gatk(args)
+	main_trim(args)
+	args.bam = args.prefix+".bqsr.bam" if args.bqsr_vcf else args.prefix+".bam"
+	main_map(args)
+	main_gatk(args)
 
 parser = argparse.ArgumentParser(description='fastq2matrix pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 subparsers = parser.add_subparsers(help="Task to perform")
@@ -68,6 +68,6 @@ parser_sub.set_defaults(func=main_gatk)
 
 args = parser.parse_args()
 if vars(args) == {}:
-    parser.print_help(sys.stderr)
+	parser.print_help(sys.stderr)
 else:
-    args.func(args)
+	args.func(args)
