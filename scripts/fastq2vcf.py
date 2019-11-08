4599ce9d815537491a6fae06cb4b6981ec8d1a2b
jodyphelan <jody.phelan@lshtm.ac.uk>
2019-11-08 09:55:23 +0100

    add redo option;

--- scripts/fastq2vcf.py
+++ scripts/fastq2vcf.py
@@ -9,13 +9,14 @@ def main_trim(args):
 	fm.run_cmd("trimmomatic PE %(read1)s %(read2)s -baseout %(prefix)s LEADING:3 TRAILING:3 SLIDINGWINDOW:4:20 MINLEN:36" % vars(args))
 
 def main_map(args):
-	fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(prefix)s_1P %(prefix)s_2P | samtools view -@ %(threads)s -b -o %(prefix)s.bam - " % vars(args))
-	fm.run_cmd("gatk MarkDuplicatesSpark -I %(prefix)s.bam -O %(prefix)s.mkdup.bam -M %(prefix)s.marked_dup_metrics.txt --conf 'spark.executor.cores=%(threads)s'" % vars(args))
-	fm.run_cmd("samtools sort -@ %(threads)s -o %(prefix)s.mkdup.sort.bam %(prefix)s.mkdup.bam" % vars(args))
-	fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.mkdup.sort.bam" % vars(args))
-	fm.run_cmd("rm %(prefix)s_1P %(prefix)s_2P %(prefix)s_1U %(prefix)s_2U" % vars(args))
-	fm.run_cmd("rm %(prefix)s.bam* %(prefix)s.mkdup.bam*" % vars(args))
-	if args.bqsr_vcf:
+	if args.redo and args.step<1:
+		fm.run_cmd("bwa mem -t %(threads)s -R \"@RG\\tID:%(prefix)s\\tSM:%(prefix)s\\tPL:Illumina\" %(ref)s %(prefix)s_1P %(prefix)s_2P | samtools view -@ %(threads)s -b -o %(prefix)s.bam - " % vars(args))
+		fm.run_cmd("gatk MarkDuplicatesSpark -I %(prefix)s.bam -O %(prefix)s.mkdup.bam -M %(prefix)s.marked_dup_metrics.txt --conf 'spark.executor.cores=%(threads)s'" % vars(args))
+		fm.run_cmd("samtools sort -@ %(threads)s -o %(prefix)s.mkdup.sort.bam %(prefix)s.mkdup.bam" % vars(args))
+		fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.mkdup.sort.bam" % vars(args))
+		fm.run_cmd("rm %(prefix)s_1P %(prefix)s_2P %(prefix)s_1U %(prefix)s_2U" % vars(args))
+		fm.run_cmd("rm %(prefix)s.bam* %(prefix)s.mkdup.bam*" % vars(args))
+	if args.bqsr_vcf and args.redo and args.step<2:
 		fm.run_cmd("gatk BaseRecalibrator -R %(ref)s -I %(prefix)s.mkdup.sort.bam --known-sites %(bqsr_vcf)s -O %(prefix)s.recal_data.table" % vars(args))
 		fm.run_cmd("gatk ApplyBQSR -R %(ref)s -I %(prefix)s.mkdup.sort.bam --bqsr-recal-file %(prefix)s.recal_data.table -O %(prefix)s.bqsr.bam" % vars(args))
 		fm.run_cmd("samtools index -@ %(threads)s %(prefix)s.bqsr.bam" % vars(args))
@@ -28,10 +29,18 @@ def main_gatk(args):
 	fm.run_cmd("gatk ValidateVariants -V %(prefix)s.g.vcf.gz -gvcf -R %(ref)s && touch %(prefix)s.g.vcf.gz.validated" % vars(args))
 
 def main_all(args):
-	main_trim(args)
+	files = {f"{args.prefix}.mkdup.sort.bam.bai":1,f"{args.prefix}.bqsr.bam.bai":2,f"{args.prefix}.g.vcf.gz.validated":3}
+	args.step = 0
+	for f in files:
+		if fm.isfile(f): args.step = files[f]
 	args.bam = args.prefix+".bqsr.bam" if args.bqsr_vcf else args.prefix+".bam"
-	main_map(args)
-	main_gatk(args)
+
+	if args.redo or args.step<1:
+		main_trim(args)
+	if args.redo or args.step<2:
+		main_map(args)
+	if args.redo or args.step<3
+		main_gatk(args)
 
 parser = argparse.ArgumentParser(description='fastq2matrix pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 subparsers = parser.add_subparsers(help="Task to perform")
@@ -43,6 +52,7 @@ parser_sub.add_argument('--prefix','-p',help='Sample prefix for all results gene
 parser_sub.add_argument('--ref','-r',help='Second read file',required=True)
 parser_sub.add_argument('--threads','-t',default=4,help='Number of threads')
 parser_sub.add_argument('--bqsr-vcf','-q',help='VCF file used for bqsr')
+parser_sub.add_argument('--redo',action="store_true",help='Redo everything')
 parser_sub.set_defaults(func=main_all)
 
 parser_sub = subparsers.add_parser('trim', help='Trim reads using trimmomatic', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
