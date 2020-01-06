a6a6f0fe7e247c96ba7a3791e8f49ca232abf148
Jody Phelan <jody.phelan@lshmt.ac.uk>
2020-01-06 13:45:35 +0000

    allow non tb chromosomes

--- scripts/merge_vcfs.py
+++ scripts/merge_vcfs.py
@@ -47,9 +47,8 @@ def main(args):
     if nofile("%s.fai" % args.ref.replace(".fasta","").replace(".fa","")):
         run_cmd("samtools faidx %(ref)s" % params)
     if nofolder("%(prefix)s_genomics_db" % params) or stages[args.redo]<=1:
-        contigs = get_contigs_from_fai(args.ref+".fai")
-        params["contigs"] = ",".join(contigs)
-        run_cmd("gatk GenomicsDBImport --genomicsdb-workspace-path %(prefix)s_genomics_db -L %(contigs)s --sample-name-map %(map_file)s --reader-threads %(threads)s --batch-size 500" % params, verbose=2)
+        run_cmd("cut -f1 %(ref)s > intervals.list" % params)
+        run_cmd("gatk GenomicsDBImport --genomicsdb-workspace-path %(prefix)s_genomics_db -L intervals.list --sample-name-map %(map_file)s --reader-threads %(threads)s --batch-size 500" % params, verbose=2)
     if nofile("%(prefix)s.raw.vcf.gz" % params) or stages[args.redo]<=2:
         run_cmd("gatk --java-options \"-Xmx40g\" GenotypeGVCFs -R %(ref)s -V gendb://%(prefix)s_genomics_db -O %(prefix)s.raw.vcf.gz" % params, verbose=2)
 
