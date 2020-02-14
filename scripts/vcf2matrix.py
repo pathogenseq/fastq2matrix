2c06eac3940ab61d9ba4573ed2ce21806286a012
juuzia <emilia.manko@gmail.com>
2020-02-14 11:25:03 +0000

    Import filecheck() from f2m. Add iupacgt and no-iupacgt args handling two versions of output matrix.

--- scripts/vcf2matrix.py
+++ scripts/vcf2matrix.py
@@ -4,6 +4,7 @@ import subprocess
 import argparse
 import random
 import os
+from fastq2matrix import filecheck
 rand_generator = random.SystemRandom()
 
 def get_random_file(prefix = None,extension=None):
@@ -82,21 +83,30 @@ class vcf_class:
 		for l in open(self.temp_file):
 			self.samples.append(l.rstrip())
 		os.remove(self.temp_file)
-	def vcf_to_matrix(self,):
+	def vcf_to_matrix(self, iupacgt=True):
 		self.matrix_file = self.prefix+".mat"
 		self.binary_matrix_file = self.prefix+".mat.bin"
+
 		O = open(self.matrix_file,"w").write("chr\tpos\tref\t%s\n" % ("\t".join(self.samples)))
-		run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%IUPACGT]\\n' %(filename)s | tr '|' '/' | sed 's/\.\/\./N/g' >> %(matrix_file)s" % vars(self))
+		if args.iupacgt:
+			run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%IUPACGT]\\n' %(filename)s | tr '|' '/' | sed 's/\.\/\./N/g' >> %(matrix_file)s" % vars(self))
+
+		else:
+			self.matrix_file = self.prefix+".noniupac.vol2.mat"	
+			run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%TGT]\\n' %(filename)s | sed 's/\.\/./N/g; s/\([ACTG]\)\///g; s/|//g' | sed -r 's/([ACGT])\\1+/\\1/g' > %(matrix_file)s" % vars(self))		
+
 		O = open(self.binary_matrix_file,"w").write("chr\tpos\tref\t%s\n" % ("\t".join(self.samples)))
-		run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%GT]\\n' %(filename)s | tr '|' '/' | sed 's/\.\/\./N/g' | sed 's/0\/1/0.5/g' | sed 's/1\/1/1/g' | sed 's/0\/0/0/g' >> %(binary_matrix_file)s" % vars(self))
+		run_cmd("bcftools query -f '%%CHROM\\t%%POS\\t%%REF[\\t%%GT]\\n' %(filename)s | tr '|' '/' | sed 's/\.\/\./N/g' | sed 's/0\/1/0.5/g' | sed 's/1\/1/1/g' | sed 's/0\/0/0/g' > %(binary_matrix_file)s" % vars(self))
 
 def main(args):
 	if nofile(args.vcf): quit("Can't find %s... Exiting!" % args.vcf)
 	vcf = vcf_class(args.vcf)
-	vcf.vcf_to_matrix()
+	vcf.vcf_to_matrix(args.iupacgt)
 
 parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('--vcf',help='VCF file',required=True)
+parser.add_argument('--iupacgt', dest='iupacgt', action='store_true')
+parser.add_argument('--no-iupacgt', dest='iupacgt', action='store_false')
 parser.add_argument('--threads',default=4, type=int, help='Number of threads for parallel operations')
 parser.set_defaults(func=main)
 
