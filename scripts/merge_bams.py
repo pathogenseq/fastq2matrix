3fcf6a94282022c72548eee49cbe52ca67c53d65
jodyphelan <jody.phelan@lshtm.ac.uk>
2020-03-27 12:34:09 +0000

    add merge_bams.py

---
+++ scripts/merge_bams.py
@@ -0,0 +1,42 @@
+import fastq2matrix as fm
+import argparse
+import sys
+
+def main(args):
+    if args.prefix:
+        individual_bams = ["%s/%s%s" % (args.dir,run,args.suffix) for run in args.prefix.split("_")]
+        new_id = args.new_id if args.new_id else args.prefix
+    elif args.bams:
+        individual_bams = args.bams.split(",")
+        new_id = args.new_id if args.new_id else "_".join([bam.split("/")[-1].replace(args.suffix,"") for bam in individual_bams])
+    elif (not args.prefix and not args.bams) or (args.prefix and args.bams):
+        sys.stderr.write("Need wither '--bams' or '--prefix'... Exiting!\n")
+        quit()
+    new_bamfile = "%s/%s%s" % (args.dir,new_id,args.suffix)
+    tmp_bamfile = fm.get_random_file()
+    tmp_file = fm.get_random_file()
+    with open(tmp_file,"w") as O:
+        for l in fm.cmd_out("samtools view -H %s" % individual_bams[0]):
+            row = l.strip().split("\t")
+            if row[0]=="@RG":
+                continue
+                row[1] = "ID:%s" % new_id
+                row[2] = "SM:%s" % new_id
+            O.write("%s\n" % "\t".join(row))
+
+    fm.run_cmd("samtools merge -f - %s | samtools reheader -i %s - | samtools addreplacerg - -r 'ID:%s\\tSM:%s\\tPL:Illumina' -o %s" % (
+        " ".join(individual_bams), tmp_file, new_id, new_id, new_bamfile)
+    )
+    fm.run_cmd("samtools index %s" % new_bamfile)
+    fm.rm_files([tmp_file,tmp_bamfile])
+
+parser = argparse.ArgumentParser(description='TBProfiler pipeline',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--bams',help='Comma seperated BAM file list')
+parser.add_argument('--new-id',help='New id for BAM')
+parser.add_argument('--prefix',help='Prefix')
+parser.add_argument('--suffix',default=".bqsr.bam",help='BAM file suffix')
+parser.add_argument('--dir',default=".",help='Directory containing bams')
+parser.add_argument('--threads',default=4, type=int, help='Number of threads for parallel operations')
+parser.set_defaults(func=main)
+args = parser.parse_args()
+args.func(args)
