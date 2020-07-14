b9d1e1530fff6ac837f9ddd6199ebc756b272495
Jody Phelan <jody.phelan@lshtm.ac.uk>
2020-07-14 13:35:24 +0100

    add tb filter script

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 
     name="fastq2matrix",
-    version="0.1.2",
+    version="0.1.3",
     packages=["fastq2matrix"],
     license="MIT",
     long_description="Utilities to get from fastq files to a variant matrix",
